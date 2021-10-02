e3a5cb53fd856ad11bf6b2e7a5af905133c45016
hcm <congminh.job@gmail.com>
2021-10-02 08:59:08 +0700

    Init project

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavigationBar from './Nav';
+
+function App() {
+  return (
+    <div class="App">
+      <NavigationBar></NavigationBar>
+      <div class="content"></div>
+      <div class="footer">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
